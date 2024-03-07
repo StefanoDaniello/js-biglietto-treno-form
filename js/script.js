902317de01df22d0b341817e6d147b707f0b63da
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-03-07 15:59:20 +0100

    javascript base

--- js/script.js
+++ js/script.js
@@ -7,3 +7,29 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del bigliett
 - va applicato uno sconto del 20% per i minorenni
 - va applicato uno sconto del 40% per gli over 65.
 */
+
+let elUsernome = document.getElementById('userNome');
+
+let elKilometers = document.getElementById('kilometers');
+
+let elAge = document.getElementById('age')
+
+let elButton = document.getElementById('generate');
+
+console.log(elUsernome, elKilometers, elAge, elButton);
+
+let elBox = document.querySelector('.box');
+let elNome =  document.getElementById('nome');
+console.log(elBox,elNome)
+
+
+elButton.addEventListener('click', function(){
+    // console.log('hai cliccato il bottone');
+    const valuenome = elUsernome.value;
+    const km = elKilometers.value;
+    const age = elAge.value;
+    console.log(valuenome, km, age);
+
+    elNome.innserHTML = valuenome;
+    
+})
